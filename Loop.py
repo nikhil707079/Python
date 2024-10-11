44c7d4e09bc28eea1fadb28018951e88a9a65417
nikhil707079 <nikhil7371092582@gmail.com>
2024-10-11 07:31:08 +0530

    Add files via upload

---
+++ Loop.py
@@ -0,0 +1,30 @@
+##Counting
+##count=1
+##while count<=100:
+##    print(count)
+##    count +=1
+##
+##
+##Back Counting
+##count=100
+##while count>=1:
+##    print(count)
+##    count -=1
+##
+##
+##
+##
+## Table
+
+####count=1
+##while count<=10:
+##    print(4*count)
+##    count +=1
+##
+##
+
+n=int(input("Enter number:"))
+i=1
+while i<=10:
+    print(n*i)
+    i +=1
